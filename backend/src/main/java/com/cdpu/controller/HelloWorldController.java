1458472f128bfb17331d922875fd12a34e47db2b
Rui Richard Blaese <ruiblaese@gmail.com>
2019-12-02 19:36:55 -0300

    rest para testes hello world

---
+++ backend/src/main/java/com/cdpu/controller/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.cdpu.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cdpu.response.Response;
+
+
+@RestController
+@RequestMapping("hello")
+public class HelloWorldController {
+
+	@GetMapping
+	public ResponseEntity<Response<String>> hello(){
+			
+		Response<String> response = new Response<String>();
+		response.setData("hello world");
+		
+		return ResponseEntity.ok().body(response);
+	}
+	
+}
