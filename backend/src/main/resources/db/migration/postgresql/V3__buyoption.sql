c9fdc5d6a555896725d8754d71689ea8b51a636b
Rui Richard Blaese <ruiblaese@gmail.com>
2019-12-07 13:01:28 -0300

    add migration tabela buy_option

---
+++ backend/src/main/resources/db/migration/postgresql/V3__buyoption.sql
@@ -0,0 +1,13 @@
+create table buy_option(
+	id                  serial,
+	deal                integer,
+	title               varchar(100),
+	normal_price        numeric(18,2),
+	percentage_discount numeric(18,2),
+	quantity_cupom      integer,
+	quantity_sold       integer,
+	start_date          date,
+	end_date            date,
+	primary key(id),
+	foreign key(deal) references deal(id)
+);
