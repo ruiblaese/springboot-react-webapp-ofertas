f47e7d53f2d51d1f5aff8d0120d5609b7f469548
Rui Richard Blaese <ruiblaese@gmail.com>
2019-12-07 12:50:26 -0300

    fix repository query

--- backend/src/main/java/com/cdpu/repository/BuyOptionRepository.java
+++ backend/src/main/java/com/cdpu/repository/BuyOptionRepository.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.cdpu.entity.BuyOption;
 
@@ -12,7 +14,8 @@ public interface BuyOptionRepository extends JpaRepository<BuyOption, Long>{
 	List<BuyOption> findAllByDeal(Long deal);
 	
 	
-	List<BuyOption> findAllByDealAndStartDateLessThanEqualAndEndDateGreaterThanEqualAndQuantityCupomLessThanQuantitySold(Long deal, Date init, Date end);
+	@Query(value = "select buyo from BuyOption buyo where (buyo.deal.id = :deal) and (buyo.startDate <= :init) and (buyo.endDate >= :end) and (buyo.quantityCupom <= buyo.quantitySold)")
+	List<BuyOption> findAllByDealAndStartDateLessThanEqualAndEndDateGreaterThanEqualAndQuantityCupomLessThanQuantitySold(@Param("deal")  Long deal, @Param("init") Date init, @Param("end") Date end);
 	
 	List<BuyOption> findAllByDealIsNull(); 
 	
