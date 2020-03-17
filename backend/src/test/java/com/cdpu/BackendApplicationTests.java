2a28346ea493fe9b828225b58a6689b16c6886a4
Rui Richard Blaese <ruiblaese@gmail.com>
2020-03-17 17:15:53 -0300

    correct tests second attempt

--- backend/src/test/java/com/cdpu/BackendApplicationTests.java
+++ backend/src/test/java/com/cdpu/BackendApplicationTests.java
@@ -2,8 +2,14 @@ package com.cdpu;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runner.RunWith;
 
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
 	@Test
